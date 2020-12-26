42ac777e4ede58545dbabf74a3eef2f920efae63
youssef121298 <youssefmohamed22222@gmail.com>
2020-12-26 05:46:17 +0200

    done

---
+++ app/src/main/java/com/example/data_entry/baseScreen.kt
@@ -0,0 +1,11 @@
+package com.example.data_entry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class baseScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base_screen)
+    }
+}
